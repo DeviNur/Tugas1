ee4a1e5553f42c017108fd98abfbc42e8f9842d5
DeviNur <41379185+DeviNur@users.noreply.github.com>
2019-03-12 15:21:22 +0700

    Add files via upload

---
+++ Devi/tugas1.php
@@ -0,0 +1,14 @@
+<!doctype html>  
+     <html>  
+     <head>  
+   
+     </head>  
+     <body>  
+	 <?php
+	 echo "<h1> Hello World </h1>"; 
+     echo "NIM : 16.01.53.0125 <br> ";
+     echo "Nama : Devi Nur Amalina <br> "; 
+	 ?> 	
+	 
+     </body>  
+     </html> 
